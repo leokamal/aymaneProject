55fc4da38bb7f69ae2d7a8ec19108cea4b2c31f3
ELMAG <kamalelmagouri@gmail.com>
2022-05-25 22:13:38 +0100

    Init project

---
+++ AymaneProject/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AymaneProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Button btn = new Button();
+
+                btn.Font = new System.Drawing.Font("Bookman Old Style", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                btn.Name = "button"+i;
+                btn.Size = new System.Drawing.Size(151, 116);
+                btn.TabIndex = i;
+                btn.Text = "button"+i;
+                btn.UseVisualStyleBackColor = true;
+
+                //add Button to FlowLayoutPanel
+                this.flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+    }
+}
